6804d9f42d9a53e9d09d25eabafebdb02cd91c87
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-09-20 11:28:17 -0500

    added new farmer from google login

--- src/javascripts/helpers/data/userData.js
+++ src/javascripts/helpers/data/userData.js
@@ -4,7 +4,8 @@ import apiKeys from '../apiKeys.json';
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
 const getUser = (userObj) => {
-  axios.get(`${baseUrl}/farmers.json?orderBy="uid"&equalTo="${userObj.uid}"`)
+  axios
+    .get(`${baseUrl}/farmers.json?orderBy="uid"&equalTo="${userObj.uid}"`)
     .then((resp) => {
       if (Object.values(resp.data).length === 0) {
         // POST OBJECT TO FARMERS
@@ -19,6 +20,17 @@ const getUser = (userObj) => {
         // use axios.post
         // pass it the url ${baseUrl}/farmers.json
         // pass it an object
+        axios({
+          method: 'post',
+          url: `${baseUrl}/farmers.json`,
+          data: {
+            image: userObj.photoURL,
+            uid: userObj.uid,
+            displayName: userObj.displayName,
+            email: userObj.email,
+            lastSignInTime: userObj.metadata.lastSignInTime,
+          },
+        }).then(console.warn('user created'));
       } else {
         console.warn('User Exists');
       }
